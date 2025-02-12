f333bc119e737fdf9e81f4d14695893b29610416
trainer_brendan <omkarpast@gmail.com>
2025-02-12 17:09:09 +0530

    Update matrix_vector_multiplication.cu

--- Linear Algebra Operations/matrix_vector_multiplication.cu
+++ Linear Algebra Operations/matrix_vector_multiplication.cu
@@ -1,4 +1,4 @@
-// Given a matrix A of shape (M,K) and an input vector x of shape (K,1)
+// Given a matrix A of shape (M,K) and an input vector B of shape (K,1)
 
 __global__ void sgemv_kernel(int M, int K, float alpha, float *A, float *B, float beta, float *C) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
